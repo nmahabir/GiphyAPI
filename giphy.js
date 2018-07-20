25764a6e2f16da4849b12534b443cda0c984b1cc
Tals <natalia.mahabir@gmail.com>
2018-07-19 21:40:48 -0400

    removed second function call for button creator

--- giphy.js
+++ giphy.js
@@ -11,8 +11,8 @@ $(document).ready(function() {
           animalButtons[i] +
           "</button>"
       );
-    };
-  };
+    }
+  }
   buttonCreator();
 
   function giphy() {
@@ -27,58 +27,58 @@ $(document).ready(function() {
       $.ajax({
         url: queryURL,
         method: "GET"
-      }).then(function(response) {
-        var results = response.data;
-        console.log(results);
-        for (var i = 0; i < results.length; i++) {
-          var animalDiv = $("<div class='card' style='width:18rem;'>");
-          var p = $("<p>").text("Rating: " + results[i].rating);
-          var animalImage = $("<img>");
-          animalImage.attr("src", results[i].images.fixed_height.url);
-          console.log(results[i].images.fixed_height.url);
-          // onclick for images to start and stop
-          animalImage.attr("class", "gif");
-          animalImage.attr("data-state", "animate");
-          animalImage.attr(
-            "data-still",
-            results[i].images.fixed_height_still.url
-          );
-          animalImage.attr("data-animate", results[i].images.fixed_height.url);
+      })
+        .then(function(response) {
+          var results = response.data;
+          console.log(results);
+          for (var i = 0; i < results.length; i++) {
+            var animalDiv = $("<div class='card' style='width:18rem;'>");
+            var p = $("<p>").text("Rating: " + results[i].rating);
+            var animalImage = $("<img>");
+            animalImage.attr("src", results[i].images.fixed_height.url);
+            console.log(results[i].images.fixed_height.url);
+            // onclick for images to start and stop
+            animalImage.attr("class", "gif");
+            animalImage.attr("data-state", "animate");
+            animalImage.attr(
+              "data-still",
+              results[i].images.fixed_height_still.url
+            );
+            animalImage.attr(
+              "data-animate",
+              results[i].images.fixed_height.url
+            );
 
-          animalDiv.append(p);
-          animalDiv.append(animalImage);
-          $("#animalImages").prepend(animalDiv);
-          $(".gif").on("click", function() {
-            var state = $(this).attr("data-state");
-            console.log(state);
-            if (state === "still") {
-              $(this).attr("src", $(this).attr("data-animate"));
-              $(this).attr("data-state", "animate");
-            }
-            if (state === "animate") {
-              $(this).attr("src", $(this).attr("data-still"));
-              $(this).attr("data-state", "still");
-            }
-          });
-        }
-      }).catch(function(error){
-        console.log("Oops! There's an error somewhere")});
+            animalDiv.append(p);
+            animalDiv.append(animalImage);
+            $("#animalImages").prepend(animalDiv);
+            $(".gif").on("click", function() {
+              var state = $(this).attr("data-state");
+              console.log(state);
+              if (state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+              }
+              if (state === "animate") {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+              }
+            });
+          }
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
     });
   }
 
   giphy();
-  buttonCreator();
 
   $("#search").on("click", function() {
     console.log($("#animal").val());
-    if (animalButtons.includes($("#animal").val())) {
-      $("#animal").val("");
-      buttonCreator();
-      giphy();
-    } else {
+    if (!animalButtons.includes($("#animal").val())) {
       animalButtons.push($("#animal").val());
       $("#animal").val("");
-      console.log(animalButtons);
       buttonCreator();
       giphy();
     }
